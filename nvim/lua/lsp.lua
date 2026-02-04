f16dc5b089e49e2c43cc306effb07656f88249dc
LeonardoBringel <leo.sbringel@gmail.com>
2026-02-04 01:01:05 -0300

    Update lsp config to use nvim native lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,7 +1,7 @@
 -- Lua LSP
 -- brew install lua-language-server
-require("lspconfig").lua_ls.setup({})
+vim.lsp.config('lua_ls', {})
 
 -- Python LSP
 -- install pip install pyright
-require("lspconfig").pyright.setup({})
+vim.lsp.config('pyright', {})
