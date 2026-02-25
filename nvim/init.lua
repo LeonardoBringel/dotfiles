5f87daabedd44183aca6d5f15f9e8d5c1ae669a1
LeonardoBringel <leo.sbringel@gmail.com>
2026-02-25 00:40:52 -0300

    Disable unnecessary vim dependencies to improve startup performance

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,12 @@
+-- disable unnecessary dependencies
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_2html_plugin = 1
+
 require("keymaps")
 require("options")
 require("plugins.lazy")
