98e643e56a731f78ab9fc5312264df5375fdcfa4
LeonardoBringel <leo.sbringel@gmail.com>
2026-02-25 00:24:52 -0300

    Modify telescope plugin to latest version

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
-return { 
+return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
-  dependencies = { "nvim-lua/plenary.nvim" }
+  version="*",
+  dependencies={ "nvim-lua/plenary.nvim" }
 }
 
